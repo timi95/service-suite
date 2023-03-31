7b5c6ba7d2a65a757e6be05aa0326a15960a3b11
Timi <timi.ogunkeye@gmail.com>
2023-03-31 18:33:28 +0100

    multiple instances of inventory service enabled, but OrderService request failing with webclientrequest exception

--- order-service/src/main/java/com/service/suite/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/service/suite/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -36,8 +36,8 @@ public class OrderService {
                 .toList();
         // Call Inventory Service, and place order if product
         // is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder
                                 .queryParam("skuCode", skuCodes).build())
                 .retrieve()
